7b2faccdea6547de9fd133d40ab32736acef85a3
Jakub Švéda <jakub.sveda@icloud.com>
2021-10-21 22:40:05 +0200

    6.0.5

---
+++ src/Ukoly/Algoritmy/ZpracovaniDatVPoli.java
@@ -0,0 +1,52 @@
+package Ukoly.Algoritmy;
+
+public class ZpracovaniDatVPoli  {
+    public static void main(String[] args) {
+        System.out.println(vyhodnotPolynom(2, new int[]{40, -11, 1}) + "\n");
+        System.out.println(indexOf(1, new int[]{2, 3, 4, 5, 6, 8, 1, 2, 3}) + "\n");
+        System.out.println(indexOfLepe(1, new int[]{1, 3, 4, 5, 6, 8, 1, 2, 3}) + "\n");
+    }
+
+    private static double vyhodnotPolynom(int vstupniCisloPolynomu, int[] koeficientyPodleMocnin) {
+        double result = 0;
+        for (int i = koeficientyPodleMocnin.length - 1; i >= 0; i--) {
+            result = result * vstupniCisloPolynomu + koeficientyPodleMocnin[i];
+        }
+
+        return result;
+    }
+
+    private static int indexOf(int hledaneCislo, int[] poleProHledani) {
+        for (int i = 0; i < poleProHledani.length; i++) {
+            if (poleProHledani[i] == hledaneCislo) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private static int indexOfLepe(int hledaneCislo, int[] poleProHledani) {
+        int index = 0;
+        while (index < poleProHledani.length && poleProHledani[index] != hledaneCislo) index++;
+        return index;
+    }
+
+    private static int indexOfSeZarazkou(int hledaneCislo, int[] poleProHledani) {
+        int i = 0;
+
+        do {
+            int lastIndex = poleProHledani.length - 1;
+            int posledni = poleProHledani[lastIndex];
+
+            if (posledni == hledaneCislo)
+                return lastIndex;
+
+            poleProHledani[poleProHledani.length-1] = hledaneCislo;
+        } while (poleProHledani[i] != hledaneCislo);{
+            i++;
+        }
+
+        return i;
+    }
+}
