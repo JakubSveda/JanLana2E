c2021057064c052d98426e8d752f7a6c733a9bc0
Jakub Švéda <jakub.sveda@icloud.com>
2021-11-05 09:20:02 +0100

    7.0.0

--- src/Lekce_2021_10_22/Clovek.java
+++ src/Lekce_2021_10_22/Clovek.java
@@ -1,6 +1,9 @@
 package Lekce_2021_10_22;
 
-public class Clovek {
+import java.text.Collator;
+import java.util.Arrays;
+
+public class Clovek implements Comparable {
     String krestniJmeno;
     String prijmeni;
 
@@ -25,28 +28,33 @@ public class Clovek {
         Clovek c5 = new Clovek("Anna", "Cihlová");
 
         Clovek[] zaci = {c1, c2, c3, c4, c5};
+        printArr(zaci);
 
-        for (Clovek c: zaci) {
-            System.out.println(c);
-        }
-
+        /*
         setridPodleKrestnihoJmena(zaci);
 
         System.out.println();
-        for (Clovek c: zaci) {
-            System.out.println(c);
-        }
+        printArr(zaci);
+
+        setridPodlePrijmeni(zaci);
+
+        System.out.println();
+        printArr(zaci);
+         */
+
+        bubbleSortKrestniJmeno(zaci);
+        System.out.println();
+        printArr(zaci);
+
     }
 
     private static Clovek[] setridPodleKrestnihoJmena(Clovek[] poleClovek) {
+        Collator col = Collator.getInstance();
+
         for (int i = 1; i < poleClovek.length; i++) {
-            if (poleClovek[i - 1].krestniJmeno.compareTo(poleClovek[i].krestniJmeno) > 0) {
+            if (poleClovek[i - 1].krestniJmeno.compareTo(poleClovek[i].krestniJmeno) >= 0) {
                 Clovek c = poleClovek[i];
-
-                for (int j = i - 1; j >= 0 ; j--) {
-                    poleClovek[j + 1] = poleClovek[j];
-                }
-
+                System.arraycopy(poleClovek, 0, poleClovek, 1, i);
                 poleClovek[0] = c;
             }
         }
@@ -55,6 +63,52 @@ public class Clovek {
     }
 
     private static Clovek[] setridPodlePrijmeni(Clovek[] poleClovek) {
+        for (int i = 1; i < poleClovek.length; i++) {
+            if (poleClovek[i - 1].prijmeni.compareTo(poleClovek[i].prijmeni) > 0) {
+                Clovek c = poleClovek[i];
+                System.arraycopy(poleClovek, 0, poleClovek, 1, i);
+                /*
+                for (int j = i - 1; j >= 0 ; j--) {
+                    poleClovek[j + 1] = poleClovek[j];
+                }
+                 */
+                poleClovek[0] = c;
+            }
+        }
+
         return poleClovek;
+    }
+
+    private static Clovek[] bubbleSortKrestniJmeno(Clovek[] arr) {
+        boolean znovu = true;
+        while (znovu) {
+            boolean currentZmena = false;
+
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i - 1].krestniJmeno.compareTo(arr[i].krestniJmeno) > 0) {
+                    Clovek c = arr[i];
+                    arr[i] = arr[i - 1];
+                    arr[i - 1] = c;
+                    currentZmena = true;
+                }
+            }
+
+            znovu = currentZmena;
+        }
+
+
+        return arr;
+    }
+
+    private static void printArr(Clovek[] arr) {
+        for (Clovek c: arr) {
+            System.out.println(c);
+        }
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Clovek c = (Clovek) o;
+        return krestniJmeno.compareTo(c.krestniJmeno);
     }
 }
