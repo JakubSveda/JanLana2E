79eed68477b0029afed075bfac769db0283d39c5
Jakub Švéda <jakub.sveda@icloud.com>
2022-05-20 09:21:26 +0200

    Halda 19.0.0

---
+++ src/Lekce_2022_05_20/Halda.java
@@ -0,0 +1,61 @@
+package Lekce_2022_05_20;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Halda {
+    private ArrayList<Integer> halda = new ArrayList<>();
+
+    public Halda() {
+    }
+
+    public void add(int number) {
+        if (halda.get(0) == null) {
+            halda.set(0, number);
+            return;
+        }
+
+        int myIdx = halda.size() - 1;
+        int parentIdx = (myIdx - 1) / 2;
+        if (halda.get(myIdx) < halda.get(parentIdx)) {
+            switchNumbersByIndex(myIdx, parentIdx);
+        } else {
+            return;
+        }
+    }
+
+    private void switchUntilCorrect(int a, int b) {
+        if (halda.get(a) < halda.get(b)) {
+
+        }
+    }
+
+    private void switchNumbersByIndex(int a, int b) {
+        int change = halda.get(a);
+        halda.set(a, halda.get(b));
+        halda.set(b, change);
+    }
+
+    public int getMin() {
+        return halda.get(0);
+    }
+
+    public void removeNumber(int number) {
+
+    }
+
+    public void removeIndex(int number) {
+
+    }
+
+    @Override
+    public String toString() {
+        return halda.toString();
+    }
+
+    public static void main(String[] args) {
+        Halda heap = new Halda();
+        heap.add(12);
+        System.out.println(heap);
+    }
+}
